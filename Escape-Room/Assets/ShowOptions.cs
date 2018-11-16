5b54182d97977bd761bd851e57840ca9db9b3356
Unknown <mauricederidder1@gmail.com>
2018-11-16 12:11:11 +0100

    Add start screen
    Start button works, option button works

---
+++ Escape-Room/Assets/ShowOptions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ShowOptions : MonoBehaviour {
+
+    public string LoadScene;
+    public Button StartButton;
+    // Use this for initialization
+    void Start()
+    {
+        StartButton.onClick.AddListener(LoadNewScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void LoadNewScene()
+    {
+        SceneManager.LoadScene(LoadScene);
+    }
+}
+
